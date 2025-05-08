3ad0a7130c042a4c3071847d1f687f85194eca46
anouther <155570107+anouther@users.noreply.github.com>
2025-05-08 17:40:45 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "not merely with passion, but with a reverence profound and eternal";
+            subtitle.innerText = "je t’aime Von, not merely with passion, but with a reverence profound and eternal.";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
